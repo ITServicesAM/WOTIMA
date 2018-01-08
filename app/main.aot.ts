d08acad1e59525909a251b3e592aed9cf861d0f4
Armin Maaßen <info@armin-maassen.de>
2018-01-08 17:20:32 +0100

    no message

--- app/main.aot.ts
+++ app/main.aot.ts
@@ -1,5 +1,7 @@
 // this import should be first in order to load some required settings (like globals and reflect-metadata)
 import { platformNativeScript } from "nativescript-angular/platform-static";
 import { AppModuleNgFactory } from './app.module.ngfactory';
+import { enableProdMode } from "@angular/core";
 
+enableProdMode();
 platformNativeScript({startPageActionBarHidden: true}).bootstrapModuleFactory(AppModuleNgFactory);
